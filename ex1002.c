49390dfc6cfd6354bae51cd1b5ac95e04d370064
pereiraamanda <jpereira.amanda@gmail.com>
2021-01-14 23:37:53 -0300

    correção de tipo

---
+++ ex1002.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include<math.h>
+int main (){
+	double raio, area, n=3.14159;
+	scanf("%lf",&raio);
+	area=n*pow(raio,2);
+	printf("A=%.4lf\n", area);
+	return 0;
+}
---
+++ ex1003.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main (){
+	int A,B,soma;
+	scanf("%d",&A);
+	scanf("%d",&B);
+	soma=A+B;
+	printf("SOMA = %d\n",soma);
+	return 0;
+}
